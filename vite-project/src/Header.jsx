6de83be9ba3d7146dee6a303ed7096ea61fb554c
maja4s91 <100201597+maja4s91@users.noreply.github.com>
2024-04-16 16:06:02 +0200

    Update Header.jsx

--- vite-project/src/Header.jsx
+++ vite-project/src/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header className="header">
       <img src="./logo-meme.png" className="header-logo" />
-      <h4 className="header-text">React Course - Project 3</h4>
+      <h4 className="header-text">React App</h4>
     </header>
   );
 };
